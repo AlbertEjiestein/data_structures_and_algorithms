6899b2513819a4ffaef36f54d156078ce6709e3b
yaoaijie <1510342484@qq.com>
2020-11-13 09:13:29 +0800

    单向链表-哨兵节点

--- period1/03-单向链表.js
+++ period1/03-单向链表.js
@@ -5,16 +5,123 @@ class Node {
   }
 }
 
+// class SingleLinkedList {
+//   constructor(){
+//     this.head = null;
+//   }
+//   // 根据value查找节点
+//   findByValue(item){
+//     let current = this.head;
+//     while(current){
+//       if(current.element === item){
+//         return current;
+//       }
+//       current = current.next;
+//     }
+//     return -1;
+//   }
+
+//   // 根据index查找节点
+//   findByIndex(index){
+//     let pos = 0;
+//     let current = this.head;
+//     while(current){
+//       if(pos === index){
+//         return current;
+//       }
+//       pos++;
+//       current = current.next;
+//     }
+//     return -1;
+//   }
+  
+//   // 查找前一个
+//   findPrev(item) {
+//     let current = this.head;
+//     while(current){
+//       if(current.next.element === item) {
+//         return current;
+//       }
+//       current = current.next;
+//     }
+//     return -1;
+//   }
+  
+//   // 增加节点
+//   append(value){
+//     const newNode = new Node(value);
+//     if(this.head == null){
+//       this.head = newNode;
+//       return;
+//     }
+//     let current = this.head;
+//     while(current.next){
+//       current = current.next;
+//     }
+//     current.next = newNode;
+//   }
+
+//   // 按位置插入节点
+//   insertByIndex(item, index) {
+//     const currentNode = this.findByIndex(index);
+//     if(currentNode === -1) {
+//       console.log('未找到插入位置');
+//       return;
+//     }
+//     let newNode = new Node(item);
+//     newNode.next = currentNode.next;
+//     currentNode.next = newNode;
+//   }
+
+//   // 按值插入节点
+//   insertByValue(newElement, element){
+//     const currentNode = this.findByValue(element);
+//     if(currentNode === -1){
+//       console.log('未找到插入位置');
+//       return;
+//     }
+//     const newNode = new Node(newElement);
+//     newNode.next = currentNode.next;
+//     currentNode.next = newNode;
+//   }
+
+//   // 删除某一节点
+//   remove(item) {
+//     let current = this.head;
+//     while(current.next) {
+//       if(current.next.element === item) {
+//         current.next = current.next.next;
+//         return true;
+//       }
+//       current = current.next;
+//     }
+//     return false;
+//   }
+
+//   // 遍历显示所有节点
+//   display() {
+//     let current = this.head;
+//     while(current) {
+//       console.log(current)
+//       current = current.next;
+//     }
+//   }
+// }
+
+
+/* 
+优化：利用哨兵节点实现单向链表
+ */
+
 class SingleLinkedList {
   constructor(){
-    this.head = null;
+    this.head = new Node('head');
   }
   // 根据value查找节点
   findByValue(item){
-    let current = this.head;
+    let current = this.head.next;
     while(current){
       if(current.element === item){
-        // console.log(current);
         return current;
       }
       current = current.next;
@@ -25,7 +132,7 @@ class SingleLinkedList {
   // 根据index查找节点
   findByIndex(index){
     let pos = 0;
-    let current = this.head;
+    let current = this.head.next;
     while(current){
       if(pos === index){
         // console.log(current);
@@ -39,10 +146,9 @@ class SingleLinkedList {
   
   // 查找前一个
   findPrev(item) {
-    let current = this.head;
+    let current = this.head.next;
     while(current){
       if(current.next.element === item) {
-        console.log(current)
         return current;
       }
       current = current.next;
@@ -53,10 +159,6 @@ class SingleLinkedList {
   // 增加节点
   append(value){
     const newNode = new Node(value);
-    if(this.head == null){
-      this.head = newNode;
-      return;
-    }
     let current = this.head;
     while(current.next){
       current = current.next;
@@ -74,7 +176,7 @@ class SingleLinkedList {
     let newNode = new Node(item);
     newNode.next = currentNode.next;
     currentNode.next = newNode;
-    console.log(currentNode)
+    console.log('插入成功')
   }
 
   // 按值插入节点
@@ -87,16 +189,15 @@ class SingleLinkedList {
     const newNode = new Node(newElement);
     newNode.next = currentNode.next;
     currentNode.next = newNode;
-    console.log(currentNode)
+    console.log('插入成功')
   }
 
   // 删除某一节点
   remove(item) {
-    let current = this.head;
+    let current = this.head.next;
     while(current.next) {
       if(current.next.element === item) {
         current.next = current.next.next;
-        console.log(current);
         return true;
       }
       current = current.next;
@@ -106,7 +207,7 @@ class SingleLinkedList {
 
   // 遍历显示所有节点
   display() {
-    let current = this.head;
+    let current = this.head.next;
     while(current) {
       console.log(current)
       current = current.next;
@@ -114,14 +215,16 @@ class SingleLinkedList {
   }
 }
 
+
 const slList = new SingleLinkedList();
 slList.append(1);
 slList.append(3);
 slList.append(4);
 slList.append(6);
-// slList.findByValue(3);
-// slList.findByIndex(2);
-// slList.findPrev(4);
-slList.insertByIndex(2,1);
-slList.insertByValue(5,6);
-// slList.remove(4)
+// console.log(slList.findByValue(1));
+// console.log(slList.findByIndex(2));
+// console.log(slList.findPrev(4));
+// slList.insertByIndex(2,1);
+// slList.insertByValue(5,6);
+// console.log(slList.remove(4));
+slList.display()
